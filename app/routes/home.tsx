cbd127528651481ca237d345f31481efec5d92d8
Vincent Corleone <vincent.corleone@live.com>
2025-08-25 11:57:44 +0100

    update: created mock resume structure & type and updated homepage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,54 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import {Link, useNavigate} from "react-router";
+import {useState} from "react";
+import NavBar from "~/components/navBar";
+import {resumes as mockResumes} from '~/constants'
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume AI Analyzer" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+    const navigate = useNavigate();
+    const [resumes, setResumes] = useState<Resume[]>(mockResumes);
+    const [loadingResumes, setLoadingResumes] = useState(false);
+
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover)]">
+      <NavBar/>
+      <section className="main-section">
+          <div className="page-heading">
+              <h1>Track your applications and resume ratings</h1>
+          </div>
+          {!loadingResumes && resumes?.length === 0 ? (
+              <h2>No resumes found. Upload your first resume to get feedback.</h2>
+          ): (
+              <h2>Review your submissions and check AI-powered feedback.</h2>
+          )}
+          {loadingResumes && (
+              <div className="flex flex-col items-center justify-center">
+                  <img src="/images/resume-scan-2.gif" className="w-[200px]" />
+              </div>
+          )}
+          {!loadingResumes && resumes.length > 0 && (
+              <div className="resumes-section">
+                  {resumes.map((resume) => (
+                      <div key={resume.id}>{resume.jobTitle}</div>
+                  ))}
+              </div>
+          )}
+
+          {!loadingResumes && resumes?.length === 0 && (
+              <div className="flex flex-col items-center justify-center mt-10 gap-4">
+                  <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
+                      Upload Resume
+                  </Link>
+              </div>
+          )}
+      </section>
+
+      {[]}
+  </main>
 }
